c171bff1ccf861fc9468981ccdc4dc0bf3599399
Дима Бойко <dimabojkoo501@gmail.com>
2025-05-29 00:31:04 +0300

    Add Test#2rrr

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  testEnvironment: "jsdom",
-  transform: {
-    '^.+\\.jsx?$': 'babel-jest',
-  },
-  transformIgnorePatterns: [
-    '/node_modules/(?!uuid)/',  // Щоб трансформувати uuid
-  ],
-};
